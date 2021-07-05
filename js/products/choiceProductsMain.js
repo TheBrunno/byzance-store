162205ce57383d5b9ab9cc95e3fd118a4e765f0d
Bruno Oliveira <brunosilvadeoliveira.10@gmail.com>
2021-07-05 16:27:31 -0300

    newsletter and display products in landing-page

--- js/products/choiceProductsMain.js
+++ js/products/choiceProductsMain.js
@@ -1,8 +1,19 @@
 const datingRingContainer = document.querySelector('.dating-ring-container')
 const engagementRingContainer = document.querySelector('.engagement-ring-container')
+let locals = [
+    [
+        "ring-dating", // type of product
+        datingRingContainer, // local
+        0 // amount
+    ],
+    [
+        "ring-engagement",
+        engagementRingContainer,
+        0
+    ],
+]
 
 function choiceProducts(){
-    let contDating = 0; let contEngagement = 0;
     for(product in allProducts){
         let template = 
         `<div class="${allProducts[product][7]}-item">
@@ -12,16 +23,12 @@ function choiceProducts(){
         <span class="price">Por: ${Number(allProducts[product][3]).toLocaleString("pt-br", {style:'currency', currency:'Brl'})}</span>
         <span class="division">${allProducts[product][4]}x de ${(Number(allProducts[product][3]) / Number(allProducts[product][4])).toLocaleString("pt-br", {style:'currency', currency:'Brl'})}</span>
         </div>`
-        if(allProducts[product][7] == "ring-dating"){
-            contDating++
-            if(contDating <= 4){
-                datingRingContainer.innerHTML += template
-            }
-            console.log(datingRingContainer.innerHTML)
-        }else if(allProducts[product][7] == "ring-engagement"){
-            contEngagement++
-            if(contEngagement <= 4){
-                engagementRingContainer.innerHTML += template
+        for(l in locals){
+            if(allProducts[product][7] == locals[l][0]){
+                locals[l][2]++
+                if(locals[l][2] <= 4){
+                    locals[l][1].innerHTML += template
+                }            
             }
         }
     }
